ab7318a3200836e7b17907c49b09296b1d5e90b4
everywherewego <964353527@qq.com>
2019-11-11 20:50:08 +0800

    修改日志布局

--- src/main/java/utils/basicutil/e_MD5Util.java
+++ src/main/java/utils/basicutil/e_MD5Util.java
@@ -8,7 +8,7 @@ public class e_MD5Util {
         try {
             MessageDigest m = MessageDigest.getInstance("MD5");
             m.update(dataStr.getBytes(StandardCharsets.UTF_8));
-            byte s[] = m.digest();
+            byte[] s = m.digest();
             StringBuilder result = new StringBuilder();
             for (byte b : s) {
                 result.append(Integer.toHexString((0x000000FF & b) | 0xFFFFFF00).substring(6));
